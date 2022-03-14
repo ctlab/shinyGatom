5fe6550db879eb0b3d0358cae475104845b47854
Alexey Sergushichev <alsergbox@gmail.com>
2022-03-14 20:00:38 +0300

    debug in run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec R -e "options(shiny.autoload.r=FALSE); shiny::runApp('.', launch.browser=F, port=8081)"
+exec R -e "options(shiny.autoload.r=FALSE,shiny.error=traceback,shiny.trace=TRUE,shiny.fullstacktrace=TRUE); shiny::runApp('.', launch.browser=F, port=8081)"
